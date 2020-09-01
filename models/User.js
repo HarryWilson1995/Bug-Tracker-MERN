313724da0a96bea68cc41196339a51f7d2e252e5
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-09-01 16:16:01 -0400

    add all fields to user schema and export

--- models/User.js
+++ models/User.js
@@ -5,4 +5,23 @@ const UserSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  admin: {
+    type: Boolean,
+    default: false,
+  },
 });
+
+module.exports = mongoose.model('user', UserSchema);
