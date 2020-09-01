9bfc750bf35e4839187a8fc3555da633c4618a51
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-09-01 16:38:25 -0400

    try finding user by email, if duplicate send error, otherwise initialize new user

--- routes/users.js
+++ routes/users.js
@@ -17,13 +17,25 @@ router.post(
       'Please enter a password with 7 or more characters'
     ).isLength({ min: 7 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('completed with valid response');
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+      user = new User({
+        name,
+        email,
+        password,
+      });
+    } catch (err) {}
   }
 );
 
