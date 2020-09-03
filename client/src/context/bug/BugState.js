a0ff28456748fad51a60a2b3b9b6480a803e2d56
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-09-03 13:20:13 -0400

    setup bugState file within context folder and set initial state. Export and wrap app component contents within bugstate

--- client/src/context/bug/BugState.js
+++ client/src/context/bug/BugState.js
@@ -2,3 +2,46 @@ import React, { useReducer } from 'react';
 import uuid from 'uuid';
 import BugContext from './bugContext';
 import bugReducer from './bugReducer';
+import {
+  ADD_BUG,
+  DELETE_BUG,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_BUG,
+  FILTER_BUGS,
+  CLEAR_BUGS,
+} from '../types';
+
+const BugState = (props) => {
+  const initialState = {
+    bugs: [],
+  };
+
+  const [state, dispatch] = useReducer(bugReducer, initialState);
+
+  // Add Bug
+
+  // Delete Bug
+
+  // Set Current Bug
+
+  // Clear Current Bug
+
+  // Update Bug
+
+  // Filter Bugs
+
+  // Clear Filter
+
+  return (
+    <BugContext.Provider
+      value={{
+        bugs: state.bugs,
+      }}
+    >
+      {props.children}
+    </BugContext.Provider>
+  );
+};
+
+export default BugState;
