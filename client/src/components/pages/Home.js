21b7a95ae191c2b286b9e70bceb42ebb64f72068
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-09-04 08:11:12 -0400

    bring filter component into home component and embed above bugs component

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Bugs from '../bugs/Bugs';
 import BugForm from '../bugs/BugForm';
+import BugFilter from '../bugs/BugFilter';
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <BugForm />
       </div>
       <div>
+        <BugFilter />
         <Bugs />
       </div>
     </div>
