1cec5a44dee038c8e576eb36bfafead49c9cfa6d
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-09-05 07:51:42 -0400

    create setAlert method in alert state which dispatches to reducer and have that method and initial state in return values

--- client/src/context/alert/AlertState.js
+++ client/src/context/alert/AlertState.js
@@ -1,6 +1,7 @@
 import React, { useReducer } from 'react';
 import AlertContext from './alertContext';
 import alertReducer from './alertReducer';
+import { v4 as uuidv4 } from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from '../types';
 
 const AuthState = (props) => {
@@ -9,9 +10,25 @@ const AuthState = (props) => {
   const [state, dispatch] = useReducer(alertReducer, initialState);
 
   // Set Alert
+  const setAlert = (msg, type, timeout = 5000) => {
+    const id = uuidv4();
+    dispatch({
+      type: SET_ALERT,
+      payload: { msg, type, id },
+    });
+
+    setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
+  };
 
   return (
-    <AlertContext.Provider value={{}}>{props.children}</AlertContext.Provider>
+    <AlertContext.Provider
+      value={{
+        alerts: state,
+        setAlert,
+      }}
+    >
+      {props.children}
+    </AlertContext.Provider>
   );
 };
 
