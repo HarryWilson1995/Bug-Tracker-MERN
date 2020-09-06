e5a1b5719b8ff971e1bfc30e45e48124a68943cb
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-09-06 07:46:13 -0400

    bring setAuthToken function into main app file and auth state file

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,16 @@ import About from './components/pages/About';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alerts from './components/layout/Alerts';
+
 import AlertState from './context/alert/AlertState';
 import BugState from './context/bug/BugState';
 import AuthState from './context/auth/AuthState';
+import setAuthToken from './utils/setAuthToken';
 import './App.css';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 const App = () => {
   return (
