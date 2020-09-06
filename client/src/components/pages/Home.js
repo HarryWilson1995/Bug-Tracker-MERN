a8b4a91ffcc0b4363e8d6d42e7f341759e202678
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-09-06 07:57:46 -0400

    added checks on homepage for token so that we can keep a user authenticated

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Bugs from '../bugs/Bugs';
 import BugForm from '../bugs/BugForm';
 import BugFilter from '../bugs/BugFilter';
+import AuthContext from '../../context/auth/authContext';
 
 const Home = () => {
+  const authContext = useContext(AuthContext);
+
+  useEffect(() => {
+    authContext.loadUser();
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <div className='grid-2'>
       <div>
